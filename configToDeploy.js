4f557ba22fe0fa26c1567eca85dd2635bf63024c
moises <angular.moises82@gmail.com>
2023-07-01 07:46:15 -0300

    elimine archivo sobrante

--- configToDeploy.js
+++
@@ -1,9 +0,0 @@
-const configToDeploy = {
-	KEY_SECRET_TOKEN : process.env.KEY_SECRET_TOKEN,
-	USER_DATABASE : process.env.USER_DATABASE,
-	PASSWORD_DATABASE : process.env.PASSWORD_DATABASE,
-	NAME_DATABASE : process.env.NAME_DATABASE
-}
-
-
-module.exports= configToDeploy
